a2b66865353c08cf1a793089b7888f2aea328749
sanket sonawane <sanket28896@gmail.com>
2023-10-05 15:35:41 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import './styles/board.css';
+import './styles/counter.css';
+import './styles/settings.css';
+import './styles/timer.css';
+import './styles/utilities.css';
+import './styles/pawn_promotion.css';
+import './styles/switch.css';
+import './styles/keyboard_control.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
